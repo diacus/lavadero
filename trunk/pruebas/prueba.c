22bc4edd517745b5dca1161e6d4cd794d8dbd730
diegonetomx <diegonetomx@f423c9c7-d748-6f78-53d1-c27ecb9ca613>
2010-05-11 04:53:30 +0000

    escribiendo una prueba para la lista

--- trunk/pruebas/prueba.c
+++ trunk/pruebas/prueba.c
@@ -5,8 +5,18 @@
  *      Author: diacus
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <prueba.h>
+#include <string.h>
 #include <thash.h>
+#include <lista.h>
+
+#define CREA_ENTERO(a) \
+	a = (int *) malloc(sizeof(int));
+
+#define BORRA_ENTERO(a) \
+	free(a);
 
 int prueba1() {
 
@@ -25,5 +35,20 @@ int prueba1() {
 }
 
 int pruebahash( int argc, char *argv[] ) {
+
+	char clave[256] = "";
+	int i, *item;
+	srand( time(NULL) );
+
+	CREA_ENTERO(item); *item = rand() % 1000;
+	lista *lis = lista_new( (void *) item, "inicio" );
+
+	for( i = 0; i < 10 ; i++ ) {
+		sprintf( clave, "item%02d", i);
+		CREA_ENTERO(item); *item = rand() % 1000;
+		lista_insert( lis, lista_new( (void *) item, clave ) );
+		clave[0] = '\0';
+	}
+
 	return 0;
 }
