10c412ea10bdd4d645b98071b051d96117eac9ff
ryuuba <ryuuba@f423c9c7-d748-6f78-53d1-c27ecb9ca613>
2010-05-13 15:57:24 +0000

    definición de tupla, cambio de struct a typedef

--- trunk/include/tupla.h
+++ trunk/include/tupla.h
@@ -8,16 +8,14 @@
 #ifndef TUPLA_H_
 #define TUPLA_H_
 
-typedef struct tp{
-	unsigned int clave;
-	void *valor;
-} tupla;
+typedef void* item;
+typedef item* tupla;
 
 /* tupla *tupla_new( unsigned int k, void *val )
  *
  */
 
-tupla *tupla_new( unsigned int k, void *val );
+tupla tupla_new( unsigned int N, ... );
 
 /* int tupla_print( tupla *t )
  *
