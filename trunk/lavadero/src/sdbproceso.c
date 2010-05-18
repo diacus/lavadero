f18f4b03e350d1f1cf9824947e18c9d48d836eff
diegonetomx <diegonetomx@f423c9c7-d748-6f78-53d1-c27ecb9ca613>
2010-05-18 15:01:59 +0000

    Comletas las funciones para empaquetar y desempaquetar las tuplas

--- trunk/lavadero/src/sdbproceso.c
+++ trunk/lavadero/src/sdbproceso.c
@@ -5,6 +5,7 @@
  *      Author: diacus
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sdbproceso.h>
@@ -19,7 +20,7 @@
 
 estado *sdbproceso_estado() {
 	static int creado = 0;
-	static estado *est;
+	static estado *est = NULL;
 
 	if( ! creado ) {
 		est = (estado *) malloc( sizeof(estado) );
@@ -46,7 +47,18 @@ char *sdbproceso_pack( unsigned int *nbytes, void *data, unsigned int *sz, char 
 	unsigned int pack_size = *sz + shift + sizeof( unsigned int );
 	char *package = (char *) calloc( pack_size, sizeof(char) );
 
-	strcpy( package, key ); /* Falta completar la implementación */
+	/* Copiando la clave de la tupla */
+	strcpy( package, key );
+
+	/* Copiando la cantidad de bytes de la tupla */
+	memcpy( package + shift, sz, sizeof(unsigned int) );
+
+	/* Copiando la tupla al paquete */
+	memcpy( package + shift + sizeof(unsigned int), data, *sz );
+
+	*nbytes = shift + sizeof(unsigned int), *sz;
+
+	return package;
 }
 
 /* void *sdbproceso_unpack( unsigned int *nbytes, char **key, char *msg )
@@ -58,4 +70,19 @@ char *sdbproceso_pack( unsigned int *nbytes, void *data, unsigned int *sz, char 
  * apuntada por key la clave con que se etiqueta.
  */
 
-void *sdbproceso_unpack( unsigned int *nbytes, char* key, char *msg );
+void *sdbproceso_unpack( unsigned int *nbytes, char **key, char *msg ) {
+
+	void *res;
+	unsigned int shift = strlen(msg) + 1;
+
+	*key = (char *) calloc( shift, sizeof(char) );
+	strcpy( *key, msg );
+
+	memcpy( nbytes, msg + shift, sizeof(unsigned int) );
+
+	res = calloc(*nbytes, sizeof(char) );
+	memcpy( res, msg + shift + sizeof(unsigned int), *nbytes );
+
+	return res;
+
+}
