a8f784032d90061cf7f93331ac2b15bad83557c5
diegonetomx <diegonetomx@f423c9c7-d748-6f78-53d1-c27ecb9ca613>
2010-05-23 16:33:14 +0000

    Dejo de invadir XD

--- trunk/trunk/lavadero/include/sdbespacio.h
+++ trunk/trunk/lavadero/include/sdbespacio.h
@@ -12,6 +12,59 @@
 #include <sdbproceso.h>
 #include <thash.h>
 
+/* TABLESIZE : Númrto de claves en las tablas hash utilizadas */
+
+#define TABLESIZE 1000
+
+/* Tipo: operacion
+ *
+ * Es una de las dos operaciones que pueden provocar el bloqueo de una
+ * tarea.
+ *
+ *   Valores
+ *     - LEER     : Cuando la tarea solicita una tupla en modo lectura.
+ *     - Eliminar : Cuando la tarea solicita una tupla en modo escritura.
+ */
+
+typedef enum oper { LEER = 200, ELIMINAR = 201 } operacion;
+
+/* Registro: pendiente
+ *
+ * Estructura para almacenar las solicigudes pendientes.
+ *
+ *   Campos
+ *     - key     : La clave de la tupla pendiente
+ *     - cliente : El identificador del cliente que solicita la tupla
+ *     - op      : La operación que el cliente quiere ejecutar sobre la tupla.
+ */
+
+typedef struct pnd {
+	char *key;
+	unsigned int cliente;
+	operacion op;
+} pendiente;
+
+/* pendiente *sdbespacio_newpendiente()
+ *
+ * Función para crear un nuevo registro de solicitudes
+ * pendientes.
+ *
+ * Devuelve un apuntador al registro recién creado.
+ */
+
+pendiente *sdbespacio_newpendiente();
+
+/* int sdbespacio_deletependiente( pendiente *p )
+ *
+ * Función para liberar el espacio de memoria donde se almacena
+ * una solicitud pendiente.
+ *
+ * Devuelve un entero igual a cero en caso de éxito, y un entero
+ * distinto de cero en caso de error.
+ */
+
+int sdbespacio_deletependiente( pendiente *p );
+
 /* thash *sdbespacio_gethash()
  *
  * Función para obtener una referencia a la tabla hash en la
@@ -41,6 +94,6 @@ int sdbespacio_start();
  * proporcionarle la tupla que solicitó.
  */
 
-int sdbespacio_atiendeGrab( char *key, unsigned int src );
+unsigned int sdbespacio_atiendeGrab( char *key, unsigned int src );
 
 #endif /* SDBMAESTRO_H_ */
