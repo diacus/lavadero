607b869f568e9c1a9c7714111a124f222f6cdf8e
ryuuba <ryuuba@f423c9c7-d748-6f78-53d1-c27ecb9ca613>
2010-05-31 22:56:24 +0000

    Aplicación Distribuida

--- trunk/trunk/lavadero/pruebas/prueba.c
+++ trunk/trunk/lavadero/pruebas/prueba.c
@@ -51,7 +51,7 @@ void coordinador(){
 	int i = 0, j;
 	TUPLA_NEW ( t, sizeof(i) );
 	TUPLA_WRITE ( t, &i);
-	for( j=0; j<3; j++){
+	for( j=0; j<1; j++){
 		sdblinda_meter( "saludo", t );
 		printf( "Coordinador: Meti Entero i = %d\n", i);
 		sdblinda_leer( "respuesta", t );
@@ -67,7 +67,7 @@ void esclavo(){
 	tupla t;
 	int i, j;
 	TUPLA_NEW( t, sizeof(i) );
-	for(j = 0; j < 3; j++) {
+	for(j = 0; j < 1; j++) {
 		sdblinda_leer( "saludo", t );
 		TUPLA_READ( &i, t );
 		printf( "Esclavo: Recibi i = %d\n", i );
